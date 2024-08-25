f10deba0cf9620b459d2f5eb5711365d255ed2d0
Sanjeevi Kumar <sanjeevik89@gmail.com>
2024-08-25 07:39:50 +0530

    refactoring

--- background.js
+++ background.js
@@ -1,5 +1,10 @@
 // service worker background.js
-chrome.tabs.onUpdated.addListener(function listener(tabId, changedProps, tab) {
+chrome.tabs.onUpdated.addListener(function listener(tabId, changedProps, tab) {  
+  // Check if tab is part of a group.
+  console.log(tab.groupId);
+  // If tab is part of a group, do nothing.
+  if (tab.groupId != -1) return;  
+
   // We are waiting for the tab we opened to finish loading.
   // Check that the tab's id matches the tab we opened,
   // and that the tab is done loading.
@@ -10,8 +15,12 @@ chrome.tabs.onUpdated.addListener(function listener(tabId, changedProps, tab) {
   console.log(tab.url);
   const url = new URL(tab.url);
   if (url.hostname === 'github.com') {
-    UpdateTabGroup('GHUB', tabId);    
+    UpdateTabGroup('GHUB', tabId);
   }
+  // Passing the above test means this is the event we were waiting for.
+  // There is nothing we need to do for future onUpdated events, so we
+  // use removeListner to stop getting called when onUpdated events fire.  
+  // chrome.tabs.onUpdated.removeListener(listener);
 });
 
 
@@ -31,6 +40,12 @@ async function UpdateTabGroup(title, tabId) {
     console.log({title}, "TabGroup does not exist");
     const groupId = await chrome.tabs.group({ tabIds: [tabId] });
     console.log({title}, "TabGroup created", groupId);
-    chrome.tabGroups.update(groupId, { title: 'GHUB' });
+    chrome.tabGroups.update(groupId, { title: 'GHUB' });    
   }
+  // focus tabId after group creation
+  chrome.tabs.update(tabId, { active: true });
+  // get windowId of tab
+  const tab = await chrome.tabs.get(tabId);
+  const windowId = tab.windowId;
+  chrome.windows.update( windowId, { focused: true, drawAttention: true, state: 'maximized' } );
 }
